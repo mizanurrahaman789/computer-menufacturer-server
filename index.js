c0fd6d80df49617189bf57bc3978fcaae561988f
Your Namemizanurrahaman789 <you@example.com>
2022-05-28 09:50:54 -0600

    server commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
@@ -12,11 +13,18 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function veryfiJWT(req, res, nest) {
+      console.log('abc')
+}
+
 async function run() {
       try {
             await client.connect();
             const partCollection = client.db('computer_menufacturer').collection('parts');
+
             const ordersCollection = client.db('computer_menufacturer').collection('orders');
+            const userCollection = client.db('computer_menufacturer').collection('users');
+            const partdetailsCollection = client.db('computer_menufacturer').collection('partdetails');
 
             app.get('/parts', async (req, res) => {
                   const query = {};
@@ -24,6 +32,7 @@ async function run() {
                   const parts = await cursor.toArray();
                   res.send(parts);
             });
+
 
             app.get('/order', async (req, res) => {
                   const parts = await partCollection.find().toArray();
@@ -34,6 +43,50 @@ async function run() {
                         const partOrders = orders.filter(orders => orders.name === parts.name);
                   })
                   res.send(orders);
+            });
+
+            app.get('./user', async (req, res) => {
+                  const users = await userCollection.find({}).toArray();
+                  res.send(users);
+            })
+
+
+            app.get('/partdetails', async (req, res) => {
+                  const partsdetails = await partdetailsCollection.find({}).toArray();
+                  console.log(partsdetails)
+                  res.send(partsdetails);
+            });
+
+            app.put('/user/admin/:email', async (req, res) => {
+                  const email = req.params.email;
+                  const user = req.body;
+                  const filter = { email: email };
+                  const options = { upsert: true };
+                  const updateDoc = {
+                        $set: admin,
+                  };
+                  const result = await userCollection.updateOne(filter, updateDoc, options);
+                  res.send({ result });
+            });
+
+            app.put('/user/:email', async (req, res) => {
+                  const email = req.params.email;
+                  const user = req.body;
+                  const filter = { email: email };
+                  const options = { upsert: true };
+                  const updateDoc = {
+                        $set: user,
+                  };
+                  const result = await userCollection.updateOne(filter, updateDoc, options);
+                  const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET,
+                        { expiresIn: '1h' });
+                  res.send({ result, token });
+            });
+
+            app.post('./partdetails', async (req, res) => {
+                  const partdetails = req.body;
+                  const result = await partdetailsCollection.insertOne(partdetails);
+                  res.send(result);
             })
 
             app.post('/orders', async (req, res) => {
