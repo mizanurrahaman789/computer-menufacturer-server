7332ac2acd235612fba7568be8402b0ecbc66980
Your Namemizanurrahaman789 <you@example.com>
2022-06-01 04:21:48 -0600

    index added

--- index.js
+++ index.js
@@ -14,7 +14,18 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 function veryfiJWT(req, res, nest) {
-      console.log('abc')
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+            return res.status(401).send({ messagge: 'Unauthorization' });
+      }
+      const token = authHeader.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (error, decoded) {
+            if (err) {
+                  return res.status(403).send({ message: 'forbiten access' })
+            }
+            req.decoded = decoded;
+            next();
+      });
 }
 
 async function run() {
@@ -24,7 +35,7 @@ async function run() {
 
             const ordersCollection = client.db('computer_menufacturer').collection('orders');
             const userCollection = client.db('computer_menufacturer').collection('users');
-            const partdetailsCollection = client.db('computer_menufacturer').collection('partdetails');
+            // const partdetailsCollection = client.db('computer_menufacturer').collection('partdetails');
 
             app.get('/parts', async (req, res) => {
                   const query = {};
@@ -37,36 +48,61 @@ async function run() {
             app.get('/order', async (req, res) => {
                   const parts = await partCollection.find().toArray();
 
-                  const orders = await ordersCollection.find().toArray();
+                  const decodedEmail = req.dedcoded.email;
+                  if (email === decodedEmail) {
 
-                  parts.forEach(parts => {
-                        const partOrders = orders.filter(orders => orders.name === parts.name);
-                  })
-                  res.send(orders);
+                        const orders = await ordersCollection.find().toArray();
+                        parts.forEach(parts => {
+                              const partOrders = orders.filter(orders => orders.name === parts.name);
+                        })
+                        return res.send(orders);
+
+                  }
+                  else {
+                        return res.status(403).send({ message: 'access forbiten' })
+                  }
+
+
             });
 
-            app.get('./user', async (req, res) => {
-                  const users = await userCollection.find({}).toArray();
+            app.get('./user', veryfiJWT, async (req, res) => {
+                  // const order = await ordersCollection.find().toArray();
+
+                  const users = await userCollection.find().toArray();
+                  // order.forEach(order => {
+                  //       // const orderUser = users.filter(users => users.name === order.name);
+                  // })
                   res.send(users);
+                  console.log(users)
+            });
+
+            app.get('admin/:email', async (req, res) => {
+                  const email = req.params.email;
+                  const user = await userCollection.findOne({ email: email });
+                  const isAdmin = user.role === 'admin';
+                  res.send({ admin: isAdmin })
             })
 
 
-            app.get('/partdetails', async (req, res) => {
-                  const partsdetails = await partdetailsCollection.find({}).toArray();
-                  console.log(partsdetails)
-                  res.send(partsdetails);
-            });
 
-            app.put('/user/admin/:email', async (req, res) => {
+
+            app.put('/user/admin/:email', veryfiJWT, async (req, res) => {
                   const email = req.params.email;
-                  const user = req.body;
-                  const filter = { email: email };
-                  const options = { upsert: true };
-                  const updateDoc = {
-                        $set: admin,
-                  };
-                  const result = await userCollection.updateOne(filter, updateDoc, options);
-                  res.send({ result });
+                  const requester = req.decoded.email;
+                  const requesterAccount = await userCollection.findOne({ email: requester });
+                  if (requesterAccount.role === 'admin') {
+
+                        const filter = { email: email };
+                        const updateDoc = {
+                              $set: { role: 'admin' },
+                        };
+                        const result = await userCollection.updateOne(filter, updateDoc);
+                        res.send({ result });
+                  }
+                  else {
+                        res.status(403).send({ message: 'forbidden' })
+                  }
+
             });
 
             app.put('/user/:email', async (req, res) => {
@@ -83,11 +119,11 @@ async function run() {
                   res.send({ result, token });
             });
 
-            app.post('./partdetails', async (req, res) => {
-                  const partdetails = req.body;
-                  const result = await partdetailsCollection.insertOne(partdetails);
-                  res.send(result);
-            })
+            // app.post('./partdetails', async (req, res) => {
+            //       const partdetails = req.body;
+            //       const result = await partdetailsCollection.insertOne(partdetails);
+            //       res.send(result);
+            // })
 
             app.post('/orders', async (req, res) => {
                   const orders = req.body;
